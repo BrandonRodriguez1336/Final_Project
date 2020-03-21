c6f5ec05036063ec9a99381c77f693f1bdb55f27
Armine Grigoryan <arminegrigor@gmail.com>
2020-03-21 11:21:21 -0700

    final adding

--- app.py
+++ app.py
@@ -1,6 +1,51 @@
-from flask import Flask, url_for, send_from_directory, request
-import logging, os
+import warnings
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Flatten, Activation
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from matplotlib.image import imread
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import classification_report, confusion_matrix
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import load_model
+from sqlalchemy import create_engine
+from sqlalchemy import create_engine, func
+from sqlalchemy.sql import text
+from sqlalchemy.orm import Session
+from sqlalchemy.ext.automap import automap_base
+import sqlalchemy
+import pandas as pd
+import numpy as np
+import datetime as dt
+from flask import Flask, jsonify, render_template, request, redirect, url_for, send_from_directory, request
+from flask_cors import CORS, cross_origin
 from werkzeug import secure_filename
+import logging
+
+import os
+import os
+
+warnings.filterwarnings('ignore')
+
+
+app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+app.config["IMAGE_UPLOADS"] = "IMAGE_UPLOADS"
+
+# session
+# engine = create_engine(
+
+#     "postgresql://test:test@localhost:5432/ProjectTwo", echo=False)
+
+
+# Base = automap_base()
+# Base.prepare(engine, reflect=True)
+# Base.classes.keys()
+# font_recognition = Base.classes.Font_recognition
+
+# session = Session(engine)
+# end session
 
 app = Flask(__name__)
 file_handler = logging.FileHandler('server.log')
@@ -18,22 +63,45 @@ def create_new_folder(local_dir):
         os.makedirs(newpath)
     return newpath
 
-@app.route('/', methods = ['POST'])
-def api_root():
-  import pdb; pdb.set_trace()
-  app.logger.info(PROJECT_HOME)
-  if request.method == 'POST' and request.files['image']:
-    app.logger.info(app.config['UPLOAD_FOLDER'])
-    img = request.files['image']
-    img_name = secure_filename(img.filename)
-    create_new_folder(app.config['UPLOAD_FOLDER'])
-    saved_path = os.path.join(app.config['UPLOAD_FOLDER'], img_name)
-    app.logger.info("saving {}".format(saved_path))
-    img.save(saved_path)
-    return send_from_directory(app.config['UPLOAD_FOLDER'],img_name, as_attachment=True)
-  else:
-    return "Where is the image?"
 
-# port
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=False)
+@app.route("/")
+def welcome():
+    return render_template("index.html")
+
+
+@app.route("/upload-image", methods=["GET", "POST"])
+# def upload_image():
+#     if request.method == "POST":
+#         if request.files:
+#             image = request.files["image"]
+#             image.save(os.path.join(
+#                 app.config["IMAGE_UPLOADS"], image.filename))
+#             print("Image saved")
+#             return redirect("/")
+def api_root():
+
+    if request.method == 'POST' and request.files['image']:
+        # app.logger.info(app.config['UPLOAD_FOLDER'])
+        img = request.files['image']  # --->>> receive the image
+        img_name = secure_filename(img.filename)
+        # ----> we create the uploads folder
+        create_new_folder(app.config['UPLOAD_FOLDER'])
+        saved_path = os.path.join(app.config['UPLOAD_FOLDER'], img_name)
+        # app.logger.info("saving {}".format(saved_path))
+        img.save(saved_path)  # ----> saves the image inside the folder
+        # score = pred_score  # ----> soon your machine learning score when u predict
+
+        # get value
+        # values = session.query(font_recognition.arial).all()
+        # list = []
+        # for value in values:
+        #     dict_values = {"Font": value[0]}
+        #     list.append(dict_values)
+        pred_score = 0
+        return "This is your predicted score " + str(pred_score)
+    else:
+        return "Where is the image?"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
